056e8fa6c64f982e697829a60b5013ae8dfcdd01
PabloDev <pe.philimon@duocuc.cl>
2026-05-16 13:52:37 -0400

    se crea el nuevo microservicio ms-recetas

---
+++ src/test/java/com/servicio_recetas/ms_recetas/MsRecetasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.servicio_recetas.ms_recetas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsRecetasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
